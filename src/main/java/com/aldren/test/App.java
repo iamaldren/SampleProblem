659940008f14765c6cb3f0c15e1ceab3bfa6a818
iamaldrew <aldrenbobiles@gmail.com>
2019-10-05 12:56:30 +0800

    Added second solution

--- src/main/java/com/aldren/test/App.java
+++ src/main/java/com/aldren/test/App.java
@@ -1,8 +1,11 @@
 package com.aldren.test;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
@@ -13,8 +16,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] A = {1,1,1,1,1,1};
+        Random rand = new Random();
 
+        int[] A = new int[20000];
+        int count = 1;
+        for(int i = 0; i < 20000; i++) {
+            A[i] = rand.nextInt(50000);
+        }
+
+        /**
+         * Original Solution
+         */
+        Instant start = Instant.now();
+        System.out.println(nestedLoop(A));
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.println("Nested Loop --> " + timeElapsed);
+
+        /**
+         * Second Solution
+         *
+         * Instead of removing the items from the List,
+         * I'll just be skipping them.
+         */
+        start = Instant.now();
+        System.out.println(secondSolution(A));
+        finish = Instant.now();
+        timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.println("Second Solution --> " + timeElapsed);
+
+    }
+
+    private static boolean nestedLoop(int[] A) {
         List<Integer> requests = new ArrayList<>();
         for(int i = 0; i < A.length; i++) {
             for(int j = i+1; j < A.length-1; j++) {
@@ -29,14 +62,39 @@ public class App
                 }
 
                 if(sum%3 == 0) {
-                    System.out.println(true);
+                    return true;
                 }
 
                 requests.clear();
             }
         }
 
-        System.out.println(false);
+        return false;
+    }
+
+    private static boolean secondSolution(int[] A) {
+        List<Integer> requests = Arrays.stream(A).boxed().collect(Collectors.toList());
+        for(int i = 0; i < A.length; i++) {
+            for(int j = i+1; j < A.length-1; j++) {
+
+                int sum = 0;
+                int counter = 0;
+                for(Integer request : requests) {
+                    if(counter != i || counter != j) {
+                        sum += request;
+                    }
+                    counter++;
+                }
+
+                if(sum%3 == 0) {
+                    return true;
+                }
+
+                requests.clear();
+            }
+        }
+
+        return false;
     }
 
 }
